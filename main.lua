9d9dba65f9decfeeeac33f4ef617009aa62fafba
Rino Jose <rjose@linkedin.com>
2013-10-17 10:21:11 -0700

    Refactor to move code to utils.lua
            - This is mainly the list view construction code

--- main.lua
+++ main.lua
@@ -1,14 +1,7 @@
-local widget = require( "widget" )
+local Utils = require('utils')
 
 display.setStatusBar( display.HiddenStatusBar )
 
---==============================================================================
--- "Constants"
---
-local LEFT_PADDING = 10
-local TITLE_GRADIENT = graphics.newGradient(
-	{ 189, 203, 220, 255 }, 
-	{ 89, 116, 152, 255 }, "down" )
 
 --==============================================================================
 -- Mock Data
@@ -41,84 +34,10 @@ local onTrackRowTouch
 local onStaffRowRender
 local onStaffRowTouch
 
---==============================================================================
--- Utils
---
-local function addRowText(row, text)
-	local rowTitle = display.newText( row, text, 0, 0, native.systemFontBold, 16 )
-	rowTitle.x = row.x - ( row.contentWidth * 0.5 ) + ( rowTitle.contentWidth * 0.5 ) + LEFT_PADDING
-	rowTitle.y = row.contentHeight * 0.5
-	rowTitle:setTextColor( 0, 0, 0 )
-
-	local rowArrow = display.newImage( row, "rowArrow.png", false )
-	rowArrow.x = row.x + ( row.contentWidth * 0.5 ) - rowArrow.contentWidth
-	rowArrow.y = row.contentHeight * 0.5
-end
-
-local function makeTitleBar()
-        -- Create toolbar to go at the top of the screen
-        local titleBar = display.newRect( 0, 0, display.contentWidth, 32 )
-        titleBar.y = display.statusBarHeight + ( titleBar.contentHeight * 0.5 )
-        titleBar:setFillColor( TITLE_GRADIENT )
-        titleBar.y = display.screenOriginY + titleBar.contentHeight * 0.5
-
-        return titleBar
-end
-
-local function makeTitle(titleBar, title)
-        local titleText = display.newEmbossedText( title, 0, 0, native.systemFontBold, 20 )
-        titleText:setReferencePoint( display.CenterReferencePoint )
-        titleText:setTextColor( 255 )
-        titleText.x = 160
-        titleText.y = titleBar.y
-
-        return titleText
-end
-
-local function makeTitleShadow(titleBar)
-        local shadow = display.newImage( "shadow.png" )
-        shadow:setReferencePoint( display.TopLeftReferencePoint )
-        shadow.x, shadow.y = 0, titleBar.y + titleBar.contentHeight * 0.5
-        shadow.xScale = 320 / shadow.contentWidth
-        shadow.alpha = 0.45
-        return shadow
-end
-
-
-
-
-
-
-local function makeList(onRowRender, onRowTouch)
-        result = widget.newTableView
-        {
-                top = 38,
-                width = 320,
-                height = 448,
-                maskFile = "mask-320x448.png",
-                onRowRender = onRowRender,
-                onRowTouch = onRowTouch,
-        }
-        return result
-end
-
-local function makeListView(title, onRowRender, onRowTouch)
-        local result = display.newGroup()
-        local list = makeList(onRowRender, onRowTouch)
-        local titleBar = makeTitleBar()
-        local titleText = makeTitle(titleBar, title)
-        local shadow = makeTitleShadow(titleBar)
-
-        result:insert( list )
-        result:insert( titleBar )
-        result:insert( titleText )
-        result:insert( shadow )
-        return result
-end
 
 local function getTrackView(tracks)
         print(tracks, tracks[1])
-        local result = makeListView("Mobile Tracks", onTrackRowRender, onTrackRowTouch);
+        local result = Utils.makeListView("Mobile Tracks", onTrackRowRender, onTrackRowTouch);
         local list = result[1]
 
         -- Add tracks
@@ -130,7 +49,7 @@ local function getTrackView(tracks)
 end
 
 local function getStaffView(track)
-        local result = makeListView(track .. " Staff", onStaffRowRender, onStaffRowTouch);
+        local result = Utils.makeListView(track .. " Staff", onStaffRowRender, onStaffRowTouch);
         local list = result[1]
 
         if trackStaff[track] then
@@ -171,7 +90,7 @@ onTrackRowRender = function( event )
 	local row = event.row
 
         local trackName = tracks[row.index]
-        addRowText(row, trackName)
+        Utils.addRowText(row, trackName)
 end
 
 
@@ -179,7 +98,7 @@ onStaffRowRender = function(event)
 	local row = event.row
 
         local personName = trackStaff[global.currentTrack][row.index]
-        addRowText(row, personName)
+        Utils.addRowText(row, personName)
 end
 
 onStaffRowTouch = function(event)
---
+++ utils.lua
@@ -0,0 +1,96 @@
+local widget = require('widget')
+
+local Utils = {}
+
+--==============================================================================
+-- "Constants"
+--
+local LEFT_PADDING = 10
+local TITLE_GRADIENT = graphics.newGradient(
+	{ 189, 203, 220, 255 }, 
+	{ 89, 116, 152, 255 }, "down" )
+
+--==============================================================================
+-- Static Declarations
+--
+local makeTitleBar
+local makeTitle
+local makeTitleShadow
+local makeList
+
+--==============================================================================
+-- Public API
+--
+Utils.addRowText = function(row, text)
+	local rowTitle = display.newText( row, text, 0, 0, native.systemFontBold, 16 )
+	rowTitle.x = row.x - ( row.contentWidth * 0.5 ) + ( rowTitle.contentWidth * 0.5 ) + LEFT_PADDING
+	rowTitle.y = row.contentHeight * 0.5
+	rowTitle:setTextColor( 0, 0, 0 )
+
+	local rowArrow = display.newImage( row, "rowArrow.png", false )
+	rowArrow.x = row.x + ( row.contentWidth * 0.5 ) - rowArrow.contentWidth
+	rowArrow.y = row.contentHeight * 0.5
+end
+
+Utils.makeListView = function(title, onRowRender, onRowTouch)
+        local result = display.newGroup()
+        local list = makeList(onRowRender, onRowTouch)
+        local titleBar = makeTitleBar()
+        local titleText = makeTitle(titleBar, title)
+        local shadow = makeTitleShadow(titleBar)
+
+        result:insert( list )
+        result:insert( titleBar )
+        result:insert( titleText )
+        result:insert( shadow )
+        return result
+end
+
+--==============================================================================
+-- Internal functions
+--
+
+makeTitleBar = function()
+        -- Create toolbar to go at the top of the screen
+        local titleBar = display.newRect( 0, 0, display.contentWidth, 32 )
+        titleBar.y = display.statusBarHeight + ( titleBar.contentHeight * 0.5 )
+        titleBar:setFillColor( TITLE_GRADIENT )
+        titleBar.y = display.screenOriginY + titleBar.contentHeight * 0.5
+
+        return titleBar
+end
+
+makeTitle = function(titleBar, title)
+        local titleText = display.newEmbossedText( title, 0, 0, native.systemFontBold, 20 )
+        titleText:setReferencePoint( display.CenterReferencePoint )
+        titleText:setTextColor( 255 )
+        titleText.x = 160
+        titleText.y = titleBar.y
+
+        return titleText
+end
+
+makeTitleShadow = function(titleBar)
+        local shadow = display.newImage( "shadow.png" )
+        shadow:setReferencePoint( display.TopLeftReferencePoint )
+        shadow.x, shadow.y = 0, titleBar.y + titleBar.contentHeight * 0.5
+        shadow.xScale = 320 / shadow.contentWidth
+        shadow.alpha = 0.45
+        return shadow
+end
+
+makeList = function(onRowRender, onRowTouch)
+        result = widget.newTableView
+        {
+                top = 38,
+                width = 320,
+                height = 448,
+                maskFile = "mask-320x448.png",
+                onRowRender = onRowRender,
+                onRowTouch = onRowTouch,
+        }
+        return result
+end
+
+
+return Utils
